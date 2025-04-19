636de7eea784d715b62a182061853f28190964b8
CorpseinAttic <dwxxynebcni@gmail.com>
2025-04-19 22:54:14 +0800

    Create netdata.py

---
+++ netdata.py
@@ -0,0 +1,18 @@
+# NetData.py
+
+net_data = {
+    "basic_nets": {
+        1: {"name": "Level 1 Net", "cost": 4000, "capacity": 10},
+        2: {"name": "Level 2 Net", "cost": 2000, "capacity": 20},
+        3: {"name": "Level 3 Net", "cost": 4000, "capacity": 30},
+        4: {"name": "Level 4 Net", "cost": 6000, "capacity": 40},
+        5: {"name": "Level 5 Net", "cost": 7000, "capacity": 50}
+    },
+    "large_nets": {
+        1: {"name": "Large Net", "cost": 12000, "capacity": 60},
+        2: {"name": "Larger Net", "cost": 16000, "capacity": 70},
+        3: {"name": "Even Larger Net", "cost": 22000, "capacity": 75},
+        4: {"name": "Even More Larger Net", "cost": 27000, "capacity": 78},
+        5: {"name": "The Largest Net", "cost": 70000, "capacity": 80}
+    }
+}
