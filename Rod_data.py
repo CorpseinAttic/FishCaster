c99edec21d16cc49330314d3c79f3e225bf254f5
CorpseinAttic <dwxxynebcni@gmail.com>
2025-04-19 22:44:58 +0800

    Create rod_data.py

---
+++ Rod_data.py
@@ -0,0 +1,9 @@
+# RodData.py
+
+rod_data = {
+    1: {"name": "Level 1 Rod", "price": 5000, "luck_multiplier": 1.7},
+    2: {"name": "Level 2 Rod", "price": 10000, "luck_multiplier": 2.7},
+    3: {"name": "Level 3 Rod", "price": 30000, "luck_multiplier": 5.0},
+    4: {"name": "Level 4 Rod", "price": 60000, "luck_multiplier": 9.0},
+    5: {"name": "Level 5 Rod", "price": 100000, "luck_multiplier": 13.5},
+}
