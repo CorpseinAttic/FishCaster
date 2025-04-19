71fc50c58429e75ed290857ef2182434820bf780
CorpseinAttic <dwxxynebcni@gmail.com>
2025-04-19 21:23:05 +0800

    fish.py
    Fishi

---
+++ fish.py
@@ -0,0 +1,46 @@
+fish_data = [ # Common {"name": "Bluegill", "rarity": "Common", "weight": 0.6, "value": 85}, {"name": "Pumpkinseed", "rarity": "Common", "weight": 0.4, "value": 70}, {"name": "Green Sunfish", "rarity": "Common", "weight": 0.5, "value": 90}, {"name": "Creek Chub", "rarity": "Common", "weight": 0.3, "value": 65}, {"name": "Shiner", "rarity": "Common", "weight": 0.2, "value": 55},
+
+# Uncommon
+{"name": "Yellow Perch", "rarity": "Uncommon", "weight": 1.2, "value": 150},
+{"name": "White Bass", "rarity": "Uncommon", "weight": 1.5, "value": 170},
+{"name": "Rock Bass", "rarity": "Uncommon", "weight": 1.3, "value": 160},
+{"name": "Warmouth", "rarity": "Uncommon", "weight": 1.1, "value": 140},
+{"name": "White Crappie", "rarity": "Uncommon", "weight": 1.4, "value": 180},
+
+# Rare
+{"name": "Peacock Bass", "rarity": "Rare", "weight": 3.4, "value": 430},
+{"name": "Spotted Seatrout", "rarity": "Rare", "weight": 2.9, "value": 350},
+{"name": "Sheepshead", "rarity": "Rare", "weight": 3.0, "value": 400},
+{"name": "Flounder", "rarity": "Rare", "weight": 3.2, "value": 480},
+{"name": "Smallmouth Buffalo", "rarity": "Rare", "weight": 3.1, "value": 450},
+
+# Epic
+{"name": "Atlantic Salmon", "rarity": "Epic", "weight": 5.6, "value": 940},
+{"name": "Northern Pike", "rarity": "Epic", "weight": 6.3, "value": 870},
+{"name": "Muskellunge", "rarity": "Epic", "weight": 7.0, "value": 990},
+{"name": "Taimen", "rarity": "Epic", "weight": 6.8, "value": 930},
+{"name": "Wels Catfish", "rarity": "Epic", "weight": 7.2, "value": 970},
+
+# Legendary
+{"name": "Bluefin Tuna", "rarity": "Legendary", "weight": 200, "value": 4700},
+{"name": "Swordfish", "rarity": "Legendary", "weight": 180, "value": 4200},
+{"name": "Marlin", "rarity": "Legendary", "weight": 210, "value": 4900},
+{"name": "Goliath Grouper", "rarity": "Legendary", "weight": 220, "value": 4650},
+{"name": "Halibut", "rarity": "Legendary", "weight": 190, "value": 4300},
+
+# Mythical
+{"name": "Opah", "rarity": "Mythical", "weight": 45, "value": 13800},
+{"name": "Giant Trevally", "rarity": "Mythical", "weight": 60, "value": 12900},
+{"name": "Arapaima", "rarity": "Mythical", "weight": 90, "value": 14000},
+{"name": "Nile Perch", "rarity": "Mythical", "weight": 100, "value": 13700},
+{"name": "Mekong Catfish", "rarity": "Mythical", "weight": 110, "value": 13500},
+
+# Divine
+{"name": "Coelacanth", "rarity": "Divine", "weight": 90, "value": 19900},
+{"name": "Giant Freshwater Stingray", "rarity": "Divine", "weight": 220, "value": 19800},
+{"name": "Paddlefish", "rarity": "Divine", "weight": 180, "value": 19000},
+{"name": "Alligator Gar", "rarity": "Divine", "weight": 160, "value": 19200},
+{"name": "Golden Dorado", "rarity": "Divine", "weight": 50, "value": 18500}
+
+]
+
